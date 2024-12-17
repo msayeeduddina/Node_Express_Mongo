06d9cc40f702d9db9ea647bcf7e4d1ef00faeb79
msayeeduddina <msayeeduddina@gmail.com>
2024-12-17 15:44:08 +0530

    url

--- Node_CWHarry/httpServer.js
+++ Node_CWHarry/httpServer.js
@@ -3,9 +3,18 @@ const http = require("http");
 const port = 8000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
+  console.log(req);
   res.setHeader("Content-Type", "text/html");
-  res.end("<h1>Hello World</h1");
+  if (req.url == "/") {
+    res.statusCode = 200;
+    res.end("<h1>Hello World Home</h1");
+  } else if (req.url == "/about") {
+    res.statusCode = 200;
+    res.end("<h1>Hello World About</h1");
+  } else {
+    res.statusCode = 404;
+    res.end("<h1>Page Not Found</h1");
+  }
 });
 
 server.listen(port, () => {
