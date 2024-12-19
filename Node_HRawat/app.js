dc2b3339d33b4dbcae8646cae169b1abf18eda3d
msayeeduddina <msayeeduddina@gmail.com>
2024-12-19 17:46:38 +0530

    require("express") & Middleware

--- Node_HRawat/app.js
+++ Node_HRawat/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const logMiddleware = require(".//logMiddleware");
+const logMiddleware = require("./Middleware/logMiddleware");
 
 const app = express();
 app.use(logMiddleware);
