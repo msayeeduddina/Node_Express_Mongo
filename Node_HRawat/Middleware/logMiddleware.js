b4bd8b462f5504f9783a6ea35eb701dd74d4f2d6
msayeeduddina <msayeeduddina@gmail.com>
2024-12-20 11:28:22 +0530

    Middleware Global and Middleware specific to route

--- Node_HRawat/Middleware/logMiddleware.js
+++ Node_HRawat/Middleware/logMiddleware.js
@@ -2,9 +2,22 @@ const express = require("express");
 
 const app = express();
 
-const logMiddleware = (req, res, next) => {
-  res.send("This is log middleware");
+const logMiddleware1 = (req, res, next) => {
+  res.send("This is log middleware1");
+  console.log("This is log middleware1");
   next();
 };
 
-module.exports = logMiddleware;
+const logMiddleware2 = (req, res, next) => {
+  res.send("This is log middleware2");
+  console.log("This is log middleware2");
+  next();
+};
+
+const logMiddleware3 = (req, res, next) => {
+  res.send("This is log middleware3");
+  console.log("This is log middleware3");
+  next();
+};
+
+module.exports = { logMiddleware1, logMiddleware2, logMiddleware3 };
--- Node_HRawat/app.js
+++ Node_HRawat/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const logMiddleware = require("./Middleware/logMiddleware");
+const {
+  logMiddleware1,
+  logMiddleware2,
+  logMiddleware3,
+} = require("./Middleware/logMiddleware");
 
 const app = express();
-app.use(logMiddleware);
+app.use(logMiddleware1);
+app.use(logMiddleware2);
+app.use("/logMW3", logMiddleware3);
 
 const port = 3000;
 
