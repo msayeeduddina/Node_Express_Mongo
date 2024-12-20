7c968c4b315ef70305eed47f8adfd465fdd6aa75
msayeeduddina <msayeeduddina@gmail.com>
2024-12-20 12:01:09 +0530

    routing

--- Node_HRawat/app.js
+++ Node_HRawat/app.js
@@ -11,23 +11,30 @@ const {
 
 const app = express();
 
-app.use(logMiddleware1); // Application-Level
-app.use(logMiddleware2);
+// app.use(logMiddleware1); // Application-Level
+// app.use(logMiddleware2);
 app.use("/logMW3", logMiddleware3); // Route-Level
-app.use((req, res, next) => {
-  // Middleware generating error
-  next(new Error("An error occurred.."));
-});
-app.use((err, req, res, next) => {
-  // Error handling middleware & it should be at last
-  console.error(err.stack);
-  res.status(500).send("Something went wrong..");
-});
+// app.use((req, res, next) => {  // Middleware generating error
+//   next(new Error("An error occurred.."));
+// });
+// app.use((err, req, res, next) => {  // Error handling middleware & it should be at last
+//   console.error(err.stack);
+//   res.status(500).send("Something went wrong..");
+// });
 app.use(express.static("public")); // Build-In Middleware
 app.use(helmet()); // Third-Party middleware - helmet: setting HTTP security headers
 app.use(bodyParser.json()); // bodyParser : Parsing request bodies
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(compression()) // compression : compressing HTTP responses
+app.use(compression()); // compression : compressing HTTP responses
+
+app.get("/", (req, res) => {
+  console.log("Hello routing GET");
+  res.send("Hello routing");
+});
+app.post("/login", (req, res) => {
+  console.log("Hello routing POST");
+  res.send("Hello routing POST");
+});
 
 const port = 3000;
 
